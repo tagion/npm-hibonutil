2e939c018572f8aa915956ea5b973a4f97bafde7
Ivan Bilan <ib@decard.io>
2023-09-18 17:35:36 +0300

    add types to package

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
-import { HiBON, HiBONType } from "./hibon/HiBON.js"; // eslint-disable-line @typescript-eslint/no-unused-vars
-import { hibonutil } from "./tagion/hibonutil.js"; // eslint-disable-line @typescript-eslint/no-unused-vars
+export { HiBON, HiBONType } from "./hibon/HiBON.js";
+export { hibonutil } from "./tagion/hibonutil.js";
