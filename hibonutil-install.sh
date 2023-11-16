ffd9700f28917fb0747f7cadd4b639699a68563c
Ivan Bilan <ib@decard.io>
2023-11-16 17:50:12 +0200

    add gh install in docker

--- hibonutil-install.sh
+++ hibonutil-install.sh
@@ -3,6 +3,15 @@
 # This script is used for Docker image to download and install
 # locally hibonutil from private repository using GITBOT_TOKEN
 
+# Install gh
+apt update && apt install -y \
+    curl \
+    gpg
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg;
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null;
+apt update && apt install -y gh;
+
+# Download and install hibonutil
 echo $GITHUB_TOKEN | gh auth login --with-token
 
 mkdir -p /usr/src/app/artifact
